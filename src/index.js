0f549a3d7605e423f7022cbe8cf1256f908bfef8
balayaydemir <balayaydemir@gmail.com>
2019-09-30 08:05:44 -0700

    api module works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+/* eslint-disable no-console */
 import $ from 'jquery';
 
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
+import api from './api';
 
 const main = function () {
- 
+  api.getItems()
+    .then(res => console.log(res));
+
+  console.log(api.BASE_URL);
 
   shoppingList.bindEventListeners();
   shoppingList.render();
